1ae60ac38fec6f65d4869a78bf22095ec1d50922
Oleh Sihunov <lancelot.lviv@gmail.com>
2021-03-15 23:14:00 +0200

    cart-item-remake

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -5,7 +5,7 @@ import { observer } from 'mobx-react';
 import ITourStore from '../../interfaces/ITourstore';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
-import CartOrder from './cartorder';
+import CartOrderItem from './cartOrderItem/cartOrderItem';
 
 const Cart = observer(() => {
   const [showModal, setState] = useState(false);
@@ -15,6 +15,7 @@ const Cart = observer(() => {
   }
   const onCheckout = () => {
     checkoutTours();
+    toggleModal();
   }
   const sumPay = selectedTours.reduce((acc, {price}) => acc + parseInt(price.replace(/[^\d]/g, '')), 0);
 
@@ -34,7 +35,7 @@ const Cart = observer(() => {
               <button onClick={toggleModal}>X</button>
             </header>
             <ol className="modal__modal-in__list">
-              {selectedTours.map((tour: ITourStore, key: number) => <CartOrder key={key} tour={tour} />)}
+              {selectedTours.map((tour: ITourStore, key: number) => <CartOrderItem key={key} tour={tour} />)}
             </ol>
             <div className="modal__modal-in__footer">
               <span className="modal__modal-in__footer__span">{sumPay}$</span>
