2650cf07fadf4345b391a0a7ab81eab436e04bf9
bogdan.paramud <bogdanp2507@gmail.com>
2021-03-16 12:08:54 +0200

    added local storage to cart

--- src/components/tours/tourCard/tourCard.tsx
+++ src/components/tours/tourCard/tourCard.tsx
@@ -10,12 +10,12 @@ interface ITourCardProps {
 }
 
 const TourCard = observer(({tour}:ITourCardProps) => {
-  const {addNewTour2, selectedTours} = rootStore.cartStore;
+  const {addNewTour, selectedTours} = rootStore.cartStore;
   const [isSelected,setSelected ]= useState(!!selectedTours.find(etour => etour.id === tour?.id) )
   const onAdd = (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault()
       if (!isSelected) {
-          addNewTour2(tour)
+          addNewTour(tour)
           setSelected(!isSelected)
       } 
     };
