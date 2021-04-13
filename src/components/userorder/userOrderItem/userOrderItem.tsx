8f281b67086dec183f3803cfc2d299a6087a2dfa
bogdan.paramud <bogdanp2507@gmail.com>
2021-04-14 00:34:19 +0300

    add userOrder & userOrderItem & change cart & all styles

--- src/components/userorder/userOrderItem/userOrderItem.tsx
+++ src/components/userorder/userOrderItem/userOrderItem.tsx
@@ -8,17 +8,16 @@ import './userOrderItem.scss';
 const UserOrderItem =({tour}:IUserOrderItemProps)=> {
     return(
         <div className="modal-user-in__container">
-            <img src={tour.imageUrl} className="modal-in__container__img" alt = 'cartholder img'/>
-            <span className="modal-in__container__title">{tour.title}</span>
-            <div className="modal-in__container__counter">
-                 <span className="modal-in__container__counter__span">{tour.personCount}</span>
+            <img src={tour.imageUrl} className="modal-user-in__container__img" alt = 'cartholder img'/>
+            <span className="modal-user-in__container__title">{tour.title}</span>
+            <div className="modal-user-in__container__counter">
+                <span className="modal-user-in__container__counter__span">{tour.personCount}</span>
             </div>
-            <div className="modal-in__container__res">
-                {console.log(tour.price + " "+tour.personCount)}
-                <span className="modal-in__container__res__span">{tour.price*tour.personCount}$</span>
+            <div className="modal-user-in__container__res">
+                <span className="modal-user-in__container__res__span">{tour.price*tour.personCount}$</span>
             </div>
         </div>
-        )
+    )
 }
 
 export default UserOrderItem;
